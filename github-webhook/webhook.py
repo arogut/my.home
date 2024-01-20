6080210831595c8544408f683f665149a9967003
arogut <adrian.rogut@gmail.com>
2024-01-20 23:21:51 +0100

    Move webhook to separate directory

--- github-webhook/webhook.py
+++ github-webhook/webhook.py
@@ -3,8 +3,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook_receiver():
-    data = request.json  # Get the JSON data from the incoming request
-    # Process the data and perform actions based on the event
+    data = request.json
     print("Received webhook data:", data)
     return jsonify({'message': 'Webhook received successfully'}), 200
 if __name__ == '__main__':
